c2e9dc42dfc65bc42866286ab6e119c4a479359f
Iván <aivan260288@gmail.com>
2021-08-11 21:43:47 -0500

    Nuevo nuevos cambios

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import Home from './Home';
 import About from './About.js';
 import Projects from './Projects';
 import Contact from './Contact';
-import { Route, Link } from "react-router-dom";
+
 import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/About" component={About} />
-      <Route exact path="/projects" component={Projects} />
-      <Route exact path="/contact" component={Contact} />
+
+      
     </div>
   );
 }
